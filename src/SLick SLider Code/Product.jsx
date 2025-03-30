d8414a4a22da59031d96be76356557655f508217
Abdullah-Adeeb <www.adib2004@gmail.com>
2025-03-30 14:01:01 +0600

    updated

---
+++ src/SLick SLider Code/Product.jsx
@@ -0,0 +1,78 @@
+// Create Image , Flex , Badge , FLEx , Text at first otherwise do it without components
+
+
+
+// import React from "react";
+// import Image from "./Image";
+// import Basket from "../../src/assets/basket.png";
+// import Badge from "./Badge";
+// import Flex from "./Flex";
+// import Text from "./Text";
+// import { FaHeart, FaShoppingCart } from "react-icons/fa";
+// import { BsArrowRepeat } from "react-icons/bs";
+// const Product = () => {
+//   return (
+//     <>
+//       <div className="relative pr-6 group">
+//         <Image imgSrc={Basket} className={"w-full"} />
+//         <Badge badgeT={"10%"} className={"absolute top-6 left-6"} />
+//         <div className="py-[33px] bg-white absolute bottom-16 left-0 w-full hidden group-hover:block">
+//           <div className="">
+//             <Flex className={"gap-x-3 justify-end pb-3 pr-5"}>
+//               <Text
+//                 as={"p"}
+//                 className={
+//                   "text-[16px] text-[#767676] hover:text-[#262626] hover:font-bold"
+//                 }
+//                 text={"Add to Wish List"}
+//               />
+//               <FaHeart />
+//             </Flex>
+//           </div>
+//           <div className="">
+//             <Flex className={"gap-x-3 justify-end py-3 pr-5"}>
+//               <Text
+//                 as={"p"}
+//                 className={
+//                   "text-[16px] text-[#767676] hover:text-[#262626] hover:font-bold"
+//                 }
+//                 text={"Compare"}
+//               />
+//               <BsArrowRepeat />
+//             </Flex>
+//           </div>
+//           <div className="">
+//             <Flex className={"gap-x-3 justify-end py-3 pr-5"}>
+//               <Text
+//                 as={"p"}
+//                 className={
+//                   "text-[16px] text-[#767676] hover:text-[#262626] hover:font-bold"
+//                 }
+//                 text={"Add to Cart"}
+//               />
+//               <FaShoppingCart />
+//             </Flex>
+//           </div>
+//         </div>
+
+//         <div className="pt-12">
+//           <Flex className={"justify-between gap-"}>
+//             <Text
+//               as={"p"}
+//               className={"text-[20px] font-bold text-[#262626]"}
+//               text={"Basic Crew Neck Tee"}
+//             />
+
+//             <Text
+//               as={"p"}
+//               className={"text-[16px] text-[#767676]"}
+//               text={"$44.00"}
+//             />
+//           </Flex>
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Product;
